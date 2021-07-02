20c022078232ef29f53e844b012321c8a0bb6489
Ganesh <giriganesh775@gmail.com>
2021-07-02 23:38:36 +0530

    first commit

--- src/app/shared/constatnts/constants.ts
+++ src/app/shared/constatnts/constants.ts
@@ -1,5 +1,5 @@
 export const base_url:string = "https://my-json-server.typicode.com/thecodertech/dbForBank/";
 
-export const RegName:string ="[A-Za-z]{3,30}";
+export const RegName:string ="[A-Za-z,. /]{3,100}";
 export const RegContact:string ="([0-9]{10,12})";
 export const RegEmail:string ="([A-Za-z0-9_\-\.])+\@([A-Za-z_\-\.]{5})+\.([A-Za-z]{2,4})";
